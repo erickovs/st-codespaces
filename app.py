50a9f40f87102b0d62c85ec1e2b59927c987b014
erickovs <125559872+erickovs@users.noreply.github.com>
2023-06-08 18:05:14 +0000

    Hola

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 
-def main():
-    st.title("Hola worlds!")
-    st.write("This is a super cool Streamlit app.")
+st.title("Hello, Kitty!")
+st.write("Vamo a divertirno")
 
-if __name__ == '__main__':
-    main()
+
+import streamlit as st
+
+valor_slider = st.sidebar.slider("Dame un numerin", 0, 10)
+checkbox_marcado = st.checkbox("Marca si el valor es mayor que 5")
+
+if valor_slider > 5:
+    if checkbox_marcado:
+        st.write("Mayor que 5")
